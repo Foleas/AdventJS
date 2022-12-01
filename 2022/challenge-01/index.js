e3863dc994207457142f66fbed4e4b084ebd03e5
Foleas <sebastianfilipetto@gmail.com>
2022-12-01 14:47:33 -0300

    Upgrade of code challenge

--- 2022/challenge-01/index.js
+++ 2022/challenge-01/index.js
@@ -1,9 +1,17 @@
+// function wrapping(gifts) {
+//   let giftsWrapped = [];
+//   for (let i = 0; i < gifts.length; i++) {
+//     const wrap = "*".repeat(gifts[i].length + 2);
+//     giftsWrapped.push(`${wrap}\n*${gifts[i]}*\n${wrap}`);
+//   }
+//   return giftsWrapped;
+// }
+
 function wrapping(gifts) {
-  let giftsWrapped = [];
-  for (let i = 0; i < gifts.length; i++) {
-    const wrap = "*".repeat(gifts[i].split("").length + 2);
-    giftsWrapped.push(`${wrap}\n*${gifts[i]}*\n${wrap}`);
-  }
+  const giftsWrapped = gifts.map((gift) => {
+    const wrap = "*".repeat(gift.length + 2);
+    return `${wrap}\n*${gift}*\n${wrap}`;
+  });
   return giftsWrapped;
 }
 
