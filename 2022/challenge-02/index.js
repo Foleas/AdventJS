6c6b11900a38719bedfcba10f769a2316138fdc7
Foleas <sebastianfilipetto@gmail.com>
2022-12-02 18:13:22 -0300

    Challenge 02 update

--- 2022/challenge-02/index.js
+++ 2022/challenge-02/index.js
@@ -1,6 +1,6 @@
 const isLeap = (year) => new Date(year, 1, 29).getDate() === 29;
 
-function countHours(year, holidays) {
+/*function countHours(year, holidays) {
   // console.log("isLeap", isLeap(year));
   let hours = 0;
   holidays.forEach((holiday) => {
@@ -14,7 +14,27 @@ function countHours(year, holidays) {
   });
   // console.log({ hours });
   return hours;
+}*/
+
+/*
+function countHours(year, holidays) {
+  let hours = 0;
+  holidays.forEach((holiday) => {
+    const dayOfWeek = new Date(`${holiday}/${year}`).getDay();
+    if (dayOfWeek !== 0 && dayOfWeek !== 6) hours += 2;
+  });
+  return hours;
 }
+*/
+
+countHours = (year, holidays) =>
+  holidays.reduce((acc, holiday) => {
+    day = new Date(`${year}-${holiday}`).getDay();
+    if (day !== 0 && day !== 6) {
+      acc += 2;
+    }
+    return acc;
+  }, 0);
 
 const year = 2022;
 const holidays = ["01/06", "04/01", "12/25"]; // formato MM/DD
@@ -23,4 +43,4 @@ const holidays = ["01/06", "04/01", "12/25"]; // formato MM/DD
 // 04/01 es el 1 de abril, un viernes. Cuenta.
 // 12/25 es el 25 de diciembre, un domingo. No cuenta.
 
-countHours(year, holidays); // 2 días -> 4 horas extra en el año
+console.log(countHours(year, holidays)); // 2 días -> 4 horas extra en el año
