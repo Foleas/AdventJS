4de5981198410c9603d4f3b649c1ffd243db4d42
Foleas <sebastianfilipetto@gmail.com>
2022-12-05 10:03:21 -0300

    Challengue 04

---
+++ 2022/challenge-04/index.js
@@ -0,0 +1,52 @@
+const fitsInOther = (box1, box2) => {
+  return box1.l < box2.l && box1.w < box2.w && box1.h < box2.h ? true : false;
+};
+
+function fitsInOneBox(boxes) {
+  const boxesSorted = boxes.sort((a, b) => a.l - b.l);
+  //a.l-b-l && a.w-b.w && a.h-b.h
+  for (let i = 0; i < boxesSorted.length; i++) {
+    //console.log(boxesSorted[i],boxesSorted[i+1]);
+    if (boxesSorted[i + 1] === undefined) break;
+    if (!fitsInOther(boxesSorted[i], boxesSorted[i + 1])) {
+      //console.log("no fit")
+      return false;
+    }
+  }
+  return true;
+}
+
+const boxes = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+];
+
+console.log(fitsInOneBox(boxes)); // true
+
+const boxes2 = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 },
+];
+
+console.log(fitsInOneBox(boxes2)); // false
+
+const boxes3 = [
+  {
+    l: 1,
+    w: 1,
+    h: 1,
+  },
+  {
+    l: 3,
+    w: 3,
+    h: 3,
+  },
+  {
+    l: 2,
+    w: 2,
+    h: 2,
+  },
+];
+
+console.log(fitsInOneBox(boxes3)); // true
