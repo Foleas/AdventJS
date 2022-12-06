58e5abb0fa970d716c42671859243617fd924c67
Foleas <sebastianfilipetto@gmail.com>
2022-12-06 12:53:27 -0300

    Challenge 05 - a little better

--- 2022/challenge-05/index.js
+++ 2022/challenge-05/index.js
@@ -1,66 +1,44 @@
-//https://www.youtube.com/watch?v=oXHJBHCXsnw
-
-function obtenerConjuntoPotencia(Arr) {
-  if (!Array.isArray(Arr)) return false;
-
-  return Arr.reduce(
-    (acum, value) => acum.concat(acum.map((item) => [value].concat(item))),
-    [[]]
-  );
-
-  const combSumLong = Arr.reduce(
+function getMaxGifts(giftsCities, maxGifts, maxCities) {
+  const allCombinationsFilteredByMaxCities = giftsCities.reduce(
     (acum, value) => {
-      console.log("acum", acum);
-      console.log("value", value);
-
-      const newCombination = acum.map((item) => {
-        console.log("item", item);
-        const arrayTemp = [value].concat(item);
-        console.log("arrayTemp", arrayTemp);
-        return arrayTemp;
-      });
-
-      console.log("newCombination", newCombination);
-
-      return acum.concat(newCombination);
+      // IF THERE IS ONE LARGER THAN MAXGIFTS
+      if (value < maxGifts) {
+        const acumConcat = [];
+        acum.forEach((item) => {
+          // FILTER BY MAX CITIES
+          if (
+            [value].concat(item).length > 0 &&
+            [value].concat(item).length <= maxCities
+          ) {
+            acumConcat.push([value].concat(item));
+          }
+        });
+        return acum.concat(acumConcat);
+      }
+      return acum;
     },
     [[]]
   );
-}
 
-function getMaxGifts(giftsCities, maxGifts, maxCities) {
-  // GET ALL COMBINATIONS POSSIBLE
-  const allCombinations = obtenerConjuntoPotencia(giftsCities);
-  // FILTER COMBINATIONS BY MAX CITIES
-  const allCombinationsFilteredByMaxCities = allCombinations.filter(
-    (combination) => combination.length > 0 && combination.length <= maxCities
-  );
+  const allCombinations = allCombinationsFilteredByMaxCities
+    .map((combination) => combination.reduce((a, v) => (a += v), 0))
+    .filter((combination) => combination <= maxGifts)
+    .sort((a, b) => b - a);
 
-  // GET THE SUM OF EACH COMBINATION
-  const allCombinationsSum = allCombinationsFilteredByMaxCities.map(
-    (combination) => combination.reduce((a, v) => (a += v), 0)
-  );
-  // FILTER SUM BY MAXGIFTS
-  const allCombinationsFiltered = allCombinationsSum.filter(
-    (combinationSum) => combinationSum <= maxGifts
-  );
-  // SORT FROM MAJOR TO MINOR
-  const allCombinationsFilteredSorted = allCombinationsFiltered.sort(
-    (a, b) => b - a
-  );
+  console.log(allCombinations);
 
-  // RETURN FIRST OR 0
-  return allCombinationsFilteredSorted[0] || 0;
+  return allCombinations[0] || 0;
 }
 
 const giftsCities = [12, 3, 11, 5, 7];
 const maxGifts = 20;
 const maxCities = 3;
 
-console.log(getMaxGifts(giftsCities, maxGifts, maxCities)); //  [12, 3, 5] 20
-console.log(getMaxGifts([50], 15, 1)); // 0
-console.log(getMaxGifts([50], 100, 1)); // 50
-console.log(getMaxGifts([50, 70], 100, 1)); // 70
-console.log(getMaxGifts([50, 70, 30], 100, 2)); // 100
-console.log(getMaxGifts([50, 70, 30], 100, 3)); // 100
-console.log(getMaxGifts([50, 70, 30], 100, 4)); // 100
+// console.log(getMaxGifts(giftsCities, maxGifts, maxCities)); //  [12, 3, 5] 20
+// console.log(getMaxGifts([50], 15, 1)); // 0
+// console.log(getMaxGifts([50], 100, 1)); // 50
+// console.log(getMaxGifts([50, 70], 100, 1)); // 70
+// console.log(getMaxGifts([50, 70, 30], 100, 2)); // 100
+// console.log(getMaxGifts([50, 70, 30], 100, 3)); // 100
+// console.log(getMaxGifts([50, 70, 30], 100, 4)); // 100
+console.log(getMaxGifts([50, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1000, 1000));
