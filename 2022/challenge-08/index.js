a12c97ce40d858501dfd2a85b1076fe9795ae94c
Foleas <sebastianfilipetto@gmail.com>
2022-12-11 14:40:28 -0300

    Challenge  08 - better solution

--- 2022/challenge-08/index.js
+++ 2022/challenge-08/index.js
@@ -1,6 +1,24 @@
 function checkPart(part) {
-  return false;
+  const isPalindrome = part.split("").some((_c, i, arr) => {
+    let subArr = arr;
+    if (i > 0) {
+      subArr = arr.slice(0, i).concat(arr.slice(i + 1));
+    }
+    return subArr.join("") === subArr.reverse().join("") ? true : false;
+  });
+  return isPalindrome;
 }
+
+/*
+function checkPart(part) {
+  const res = [...part, ""].some((_, i, arr) => {
+    const subArr = arr.slice(0, i).concat(arr.slice(i + 1));
+
+    return subArr.join("") === subArr.reverse().join("");
+  });
+
+  return res;
+}*/
 
 console.log(checkPart("uwu")); // true
 // "uwu" es un palíndromo sin eliminar ningún carácter
